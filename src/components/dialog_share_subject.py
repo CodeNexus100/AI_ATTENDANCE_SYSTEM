223a0e827cb763c44e64cc5ab4e6ea10e9994cd7
CodeNexus100 <guptakartikey114@gmail.com>
2026-07-18 04:58:54 +0530

    Finishing touches 2

--- src/components/dialog_share_subject.py
+++ src/components/dialog_share_subject.py
@@ -8,7 +8,7 @@ import io
 def share_subject_dialog(subject_name, subject_code):
     # app_domain = "snapclass-main.streamlit.app"
     # join_url = f"{app_domain}/?join-code={subject_code}"
-    app_domain = "http://localhost:8501"
+    app_domain = "https://snapclass-ai.streamlit.app"
     join_url = f"{app_domain}/?join-code={subject_code}"
 
     st.header("Scan to Join")
