88e5da39da5fbc2a0f731adca8812b65efa1a32c
CodeNexus100 <guptakartikey114@gmail.com>
2026-07-18 01:59:40 +0530

    Added share subject link and QR code

--- src/screens/teacher_screen.py
+++ src/screens/teacher_screen.py
@@ -6,6 +6,7 @@ from src.components.header import header_dashboard
 from src.components.footer import footer_dashboard
 from src.components.dialog_create_subject import create_subject_dialog
 from src.components.subject_card import subject_card
+from src.components.dialog_share_subject import share_subject_dialog
 
 def teacher_screen():
     style_background_dashboard()
